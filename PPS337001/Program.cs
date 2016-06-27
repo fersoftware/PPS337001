d6733cc3bdd83fec113fa9d00c9b8b4e661ff442
fersoftware <fersoftware@gmail.com>
2016-06-27 16:01:58 -0300

    commit

--- PPS337001/Program.cs
+++ PPS337001/Program.cs
@@ -9,7 +9,36 @@ namespace PPS337001
     class Program
     {
         static void Main(string[] args)
+        {            
+            int[] n = { 3, 37, 70, 1, 0, 73, 7, 10 }; // numeros do vetor
+            Console.WriteLine("valor final dos impars =" + SomaImpar(n, n.Length-1));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static int SomaImpar(int[] vetor, int n)
         {
+            //Mostra os valores guardados no vetor
+            if(vetor[n] %2 != 0)
+            {
+                Console.WriteLine("valor =" + vetor[n]);
+            }            
+
+            if (n > 0 && vetor[n] % 2 != 0)
+            {
+                return vetor[n] + SomaImpar(vetor, n- 1);
+            }
+            else if (n == 0 )
+            {
+                return vetor[n];
+            }
+            else if (n > 0 && vetor[n] % 2 == 0)
+            {
+                return SomaImpar(vetor,n - 1);
+            }
+
+            return 0;
+
         }
     }
 }
